b465ac1870088673776228b4db2b1259183c80e5
Fausto Leyva <fausto.lc.29@gmail.com>
2025-07-24 08:14:00 -0600

    feat: add lazy load routing and modules

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/public/login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'public',
+    loadChildren: () => import('./public/public.module').then(m => m.PublicModule)
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule),
+    canActivate: [AuthGuard],
+    canActivateChild: [AuthGuard]
+  },
+  {
+    path: '**',
+    redirectTo: '/public/login'
+  }
+];
